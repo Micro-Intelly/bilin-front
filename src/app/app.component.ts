387096e53fa85b4442310a9fcb017420f26bcc2d
Wei-bin-Wu <weibin789123@gmail.com>
2022-09-26 21:46:54 +0200

    First test connection to backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PostService } from './services/post.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bilin-front';
+
+  posts:any;
+
+  constructor(private service:PostService) {}
+
+  ngOnInit() {
+    this.service.getPosts()
+      .subscribe(response => {
+        this.posts = response;
+      });
+  }
+
 }
